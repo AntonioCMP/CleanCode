162b06973fd1ee04ebed3fa886da1e662ab70f93
AntonioCMP <antoniocalderonmatute@gmail.com>
2025-10-04 21:40:34 -0500

    Cleaning the code of scrap_bolsa, specifying the imports, getting rid of useless variables

--- BVQInterface/scrap_bolsa.py
+++ BVQInterface/scrap_bolsa.py
@@ -1,20 +1,17 @@
 
-import os
-import time
-import datetime as dt
-from selenium import webdriver
+from os import path,remove,rename
+from time import sleep
+from datetime import date
+from selenium.webdriver import ChromeOptions,Chrome
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
 def scraping():
-    PAGE= "https://www.bolsadequito.com/uploads/estadisticas/boletines/informacion-continua/maximos-y-minimos.xls"
+
 
     download_dir = "D:\\DocumentosI\\NGE\\NGE3.0+1.0\\The grid\\Data"
     NEW_ONE= "https://www.bolsadequito.com/index.php/mercados-bursatiles/mercado-en-linea/operaciones-cerradas"
-    new_name= "operaciones-cerradas" + str(dt.date.today()) + ".csv"
+    new_name= "operaciones-cerradas" + str(date.today()) + ".csv"
 
-    chrome_options = Options()
     prefs = {
         "download.default_directory": download_dir, 
         "download.prompt_for_download": False,       
@@ -22,35 +19,35 @@ def scraping():
         "safebrowsing.enabled": True                
     }
 
-    options= webdriver.ChromeOptions()
+    options= ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_experimental_option("detach",True)
     options.add_experimental_option("prefs", prefs)
 
-    driver= webdriver.Chrome(options=options)
-    time.sleep(5)
+    driver= Chrome(options=options)
+    sleep(5)
     driver.get(url=NEW_ONE)
-    time.sleep(7)
+    sleep(7)
     iframe = driver.find_element(By.TAG_NAME, "iframe")
     driver.switch_to.frame(iframe)
     element= driver.find_element(by=By.ID,value='tb2')
     element.click()
     archive= driver.find_element(by=By.XPATH,value='//*[@id="tblOCRV_wrapper"]/div[1]/button[3]') 
     archive.click()
-    ruta_original = os.path.join(download_dir, "csv.csv")
-    time.sleep(5)
+    ruta_original = path.join(download_dir, "csv.csv")
+    sleep(5)
 
     file= open(ruta_original,mode="r")
     if len(file.read().split(",")) < 10:
         print("Nah, is empty")
         file.close()
-        os.remove(ruta_original)
+        remove(ruta_original)
 
     else:
         file.close()
-        ruta_nueva = os.path.join(download_dir,new_name)
-        time.sleep(5)
-        os.rename(ruta_original, ruta_nueva)
+        ruta_nueva = path.join(download_dir,new_name)
+        sleep(5)
+        rename(ruta_original, ruta_nueva)
 
     driver.quit()
 scraping()
